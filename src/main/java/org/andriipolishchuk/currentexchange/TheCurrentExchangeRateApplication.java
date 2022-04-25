3eb0e510d8456d7c21c373ff057cd15798315bb9
a.polishchuk <andripolishchuk@gmail.com>
2022-04-25 19:44:14 +0300

    first commit

---
+++ src/main/java/org/andriipolishchuk/currentexchange/TheCurrentExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package org.andriipolishchuk.currentexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCurrentExchangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCurrentExchangeRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/andriipolishchuk/currentexchange/TheCurrentExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andriipolishchuk.currentexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCurrentExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
