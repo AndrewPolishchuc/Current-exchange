c199cbb1e407e69bd6cd2e7baf3cd96dc0f63576
apolishchuk <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>
2022-04-26 11:08:22 +0300

    Add Swagger documentation

--- src/main/java/org/andriipolishchuk/currentexchange/config/WebFluxConfiguration.java
+++ src/main/java/org/andriipolishchuk/currentexchange/config/WebFluxConfiguration.java
@@ -1,24 +1,17 @@
 package org.andriipolishchuk.currentexchange.config;
 
-import com.fasterxml.classmate.ResolvedType;
 import com.fasterxml.classmate.TypeResolver;
-import com.fasterxml.classmate.types.ResolvedArrayType;
 import org.andriipolishchuk.currentexchange.dto.ExchangeUsdDataDto;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import java.util.List;
 
 @Configuration
-public class WebFluxConfigurationTest {
+public class WebFluxConfiguration {
     @Bean
     public WebClient getWebClient() {
         return WebClient.builder().build();
